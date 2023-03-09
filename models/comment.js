51a4d19d5615bebb6a8d30746cb38ced5c9a0222
alaekwebartholomew <alaekwebartholomew@gmail.com>
2023-03-09 22:58:30 +0100

    Created comment model

---
+++ models/comment.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const User = require("./user")
+const Post = require("./post")
+
+const commentSchema = new mongoose.Schema({
+
+    "comment": String,
+    "postId": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: Post,
+    },
+
+    "userId": {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User,
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model("Comment", commentSchema)
