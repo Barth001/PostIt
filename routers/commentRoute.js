df3af16f90a85db56f556287d597c003140a2fab
alaekwebartholomew <alaekwebartholomew@gmail.com>
2023-03-10 17:21:22 +0100

    Added get all comment of a post

--- routers/commentRoute.js
+++ routers/commentRoute.js
@@ -4,7 +4,7 @@ const CommentController = require("../controllers/commentContoller")
 const Authorization = require("../middleware/authorization")
 
 router.post('/:postId/comment', Authorization.authorize, CommentController.create)
-// router.get('/:id', PostController.getpost)
+router.get('/:postId/comment', CommentController.getAllComment)
 // router.put('/:id', PostController.update)
 // router.get('/', PostController.getPosts)
 // router.delete('/:id', PostController.delete)
--- routers/indexRoute.js
+++ routers/indexRoute.js
@@ -6,9 +6,13 @@ const userProfileRoute = require("./userProfileRoute")
 const PostRoute = require("../routers/postRoute")
 const CommentRoute = require("../routers/commentRoute")
 
+// Authorization route
 router.use("/auth", authRoute);
+// User route
 router.use("/user", userProfileRoute);
+// Post route
 router.use("/post", PostRoute);
+// Comment route
 router.use("/post", CommentRoute);
 
 module.exports = router;
